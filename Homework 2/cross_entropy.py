79e4f80abcea83f77a13a34f3108a7710f8c9176
Daksh Jotwani <dakshjay@gmail.com>
2018-10-12 19:14:15 -0400

    Add cross_entropy.py (incomplete) and HW2-specific gridworld.py

---
+++ Homework 2/cross_entropy.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+import gridworld
+
+
+def softmax(row):
+    return np.exp(row) / np.sum(np.exp(row), axis=0)
+
+
+def convert_theta_to_table(theta):
+    table = np.reshape(theta, (-1, 4))
+    return np.apply_along_axis(softmax, axis=1, arr=table)
+
+
+def generate_initial_tabular_softmax_policy():
+    return np.random.uniform(0, 1, (92,))
+
+
+# TODO TRIALS = 50
+TRIALS = 1
+WHILE_LOOP_ITERATIONS_VALUES = [100]
+K_VALUES = [300]
+K_e_VALUES = [30]
+N_VALUES = [50]
+EPSILON = 0.0001
+
+
+def execute():
+    for _ in range(TRIALS):
+        for while_hyp in WHILE_LOOP_ITERATIONS_VALUES:
+            for K_hyp in K_VALUES:
+                for K_e_hyp in K_e_VALUES:
+                    for N_hyp in N_VALUES:
+                        cross_entropy(while_hyp, K_hyp, K_e_hyp, N_hyp)
+
+
+def cross_entropy(while_limit, K, K_e, N):
+    theta = generate_initial_tabular_softmax_policy()
+    sigma = np.identity(92)
+
+    list_of__theta_k__vs__J_k_hat = []
+
+    trial_results = []
+
+    for while_i in range(while_limit):
+        print(while_i)
+        for _ in range(K):
+            theta_k = np.random.multivariate_normal(theta, sigma)
+
+            table = convert_theta_to_table(theta_k)
+
+            gridworld_results = gridworld.execute(episodes=N, policy_table=table)
+            trial_results.extend(gridworld_results)
+
+            J_k_hat = np.mean(gridworld_results)
+
+            list_of__theta_k__vs__J_k_hat.append((theta_k, J_k_hat))
+
+            # End K loop
+
+        list_of__theta_k__vs__J_k_hat.sort(key=lambda x: x[1], reverse=True)
+
+        theta_k_sum = np.sum([x for (x, _) in list_of__theta_k__vs__J_k_hat][:K_e], axis=0)
+
+        theta = 1 / K_e * theta_k_sum
+
+        summation_part = [(x - theta) * np.transpose((x - theta)) for (x, _) in
+                          list_of__theta_k__vs__J_k_hat[:K_e]]
+        sigma = 1 / (EPSILON + K_e) * (EPSILON * np.identity(92) + summation_part)
+
+        # End While loop
+
+
+if __name__ == '__main__':
+    execute()
---
+++ Homework 2/gridworld.py
@@ -0,0 +1,120 @@
+import math
+import operator
+import random
+
+import numpy as np
+
+GAMMA = 0.9
+
+ROWS, COLS = 5, 5
+
+WALLS = [(2, 2), (3, 2)]
+WATER = (4, 2)
+
+START = (0, 0)
+GOAL = (4, 4)
+
+REWARD_GOAL = 10
+REWARD_WATERS = -10
+
+UP, DOWN, LEFT, RIGHT, STAY = 'AU', 'AD', 'AL', 'AR', 'STAY'
+
+DIRECTIONS = {
+    UP: (-1, 0),
+    DOWN: (1, 0),
+    LEFT: (0, -1),
+    RIGHT: (0, 1),
+    STAY: (0, 0)
+}
+
+PROB_ACTUAL = 0.8
+PROB_LEFT = 0.85
+PROB_RIGHT = 0.9
+PROB_STAY = 1.0
+
+VEERS = {
+    UP: {
+        LEFT: DIRECTIONS[LEFT], RIGHT: DIRECTIONS[RIGHT]
+    },
+    DOWN: {
+        LEFT: DIRECTIONS[RIGHT], RIGHT: DIRECTIONS[LEFT]
+    },
+    LEFT: {
+        LEFT: DIRECTIONS[DOWN], RIGHT: DIRECTIONS[UP]
+    },
+    RIGHT: {
+        LEFT: DIRECTIONS[UP], RIGHT: DIRECTIONS[DOWN]
+    },
+}
+
+
+def tabular_softmax_policy(curr, table):
+    # Convert coordinates to the 1-23 Gridworld range
+    i = (curr[0] * ROWS) + (curr[1] + 1)
+
+    # Subtract because of obstacles in GridWorld
+    if i >= 13:
+        i -= 1
+
+    if i >= 18:
+        i -= 1
+
+    if i >= 23:
+        i -= 1
+
+    # Get row of probability values for each direction
+    row = table[i]
+
+    # Fetch a random direction based on the weighted probabilities of each direction
+    return np.random.choice([UP, DOWN, LEFT, RIGHT], p=row)
+
+
+def execute(episodes, policy_table):
+    all_rewards = []
+
+    for i in range(episodes):
+
+        curr = START
+        step = -1
+        reward = 0
+
+        while True:
+            step += 1
+
+            if step > 15:
+                all_rewards.append(reward)
+                break
+
+            direction = tabular_softmax_policy(curr, policy_table)
+
+            direction_increment_coordinates = None
+
+            rand_actual_no = random.random()
+
+            if rand_actual_no < PROB_ACTUAL:
+                direction_increment_coordinates = DIRECTIONS[direction]
+            elif PROB_ACTUAL <= rand_actual_no < PROB_LEFT:
+                direction_increment_coordinates = VEERS[direction][LEFT]
+            elif PROB_LEFT <= rand_actual_no < PROB_RIGHT:
+                direction_increment_coordinates = VEERS[direction][RIGHT]
+            elif PROB_RIGHT <= rand_actual_no < PROB_STAY:
+                direction_increment_coordinates = DIRECTIONS[STAY]
+
+            curr_temp = curr
+
+            curr_temp = tuple(map(operator.add, curr_temp, direction_increment_coordinates))
+
+            if curr_temp not in WALLS and (0 <= curr_temp[0] < ROWS) and (0 <= curr_temp[1] < COLS):
+                curr = curr_temp
+
+            if curr == WATER:
+                reward += math.pow(GAMMA, step) * REWARD_WATERS
+
+            elif curr == GOAL:
+                reward += math.pow(GAMMA, step) * REWARD_GOAL
+                all_rewards.append(reward)
+                break
+
+    all_rewards = np.array(all_rewards)
+
+    return all_rewards
