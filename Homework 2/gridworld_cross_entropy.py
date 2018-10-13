9949bb960fd1d08b3bda67be2d1cd4ecd2dacd7b
Daksh Jotwani <dakshjay@gmail.com>
2018-10-13 13:13:36 -0400

    Save each gridworld trial to disk, rather than appending it to a global array

--- Homework 2/gridworld_cross_entropy.py
+++ Homework 2/gridworld_cross_entropy.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import matplotlib.pyplot as plt
@@ -12,13 +13,14 @@ def softmax(row):
 
 
 def convert_theta_to_table(theta):
-    table = np.reshape(theta, (-1, 4))
-    return np.apply_along_axis(softmax, axis=1, arr=table)
+    return np.apply_along_axis(softmax, axis=1, arr=np.reshape(theta, (-1, 4)))
 
 
 def generate_initial_tabular_softmax_policy():
     return np.random.uniform(0, 1, (92,))
 
+
+TRIALS_DIR = 'gridworld_cross_entropy_trials'
 
 TRIALS = 20
 WHILE_LOOP_ITERATIONS_VALUES = [100]
@@ -28,21 +30,20 @@ N_VALUES = [50]
 EPSILON = 0.0001
 
 
-def execute():
-    all_results = []
+def save_trial(np_arr):
+    if not os.path.exists(TRIALS_DIR):
+        os.mkdir(TRIALS_DIR)
 
+    np.save('{}/trial_{}'.format(TRIALS_DIR, time.time()), np_arr)
+
+
+def execute():
     for trial in range(TRIALS):
         for while_hyp in WHILE_LOOP_ITERATIONS_VALUES:
             for K_hyp in K_VALUES:
                 for K_e_hyp in K_e_VALUES:
                     for N_hyp in N_VALUES:
-                        trial_results = cross_entropy(while_hyp, K_hyp, K_e_hyp, N_hyp, trial)
-                        all_results.append(trial_results)
-
-    # Save results to file
-    np.save('all_results_{}'.format(time.time()), all_results)
-
-    plot_results(all_results)
+                        save_trial(cross_entropy(while_hyp, K_hyp, K_e_hyp, N_hyp, trial))
 
 
 def plot_results(results):
@@ -72,9 +73,11 @@ def cross_entropy(while_limit, K, K_e, N, trial):
             gridworld_results = gridworld.execute(episodes=N, policy_table=table)
             trial_results.extend(gridworld_results)
 
-            J_k_hat = np.mean(gridworld_results)
+            list_of__theta_k__vs__J_k_hat.append((theta_k,
 
-            list_of__theta_k__vs__J_k_hat.append((theta_k, J_k_hat))
+                                                  # J_k_hat
+                                                  np.mean(gridworld_results)
+                                                  ))
 
             # End K loop
 
@@ -86,8 +89,10 @@ def cross_entropy(while_limit, K, K_e, N, trial):
 
         theta = 1 / K_e * theta_k_sum
 
-        summation_part = [(x - theta) * np.transpose((x - theta)) for x in filtered_theta_k_list]
-        summation_part = np.sum(summation_part, axis=0)
+        summation_part = np.sum(
+            [(x - theta) * np.transpose((x - theta)) for x in filtered_theta_k_list],
+            axis=0
+        )
 
         sigma = (1 / (EPSILON + K_e)) * ((EPSILON * np.identity(92)) + summation_part)
 
