8d860474fac3e290ede58e03d2cea74fc1deea05
Daksh Jotwani <dakshjay@gmail.com>
2018-10-13 16:14:29 -0400

    Add plot_trials_from_dir.py to plot data from all trials stored as rows in each file

---
+++ Homework 2/plot_trials_from_dir.py
@@ -0,0 +1,19 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_dir(trials_dir):
+    results = []
+
+    for trial in os.listdir(trials_dir):
+        results.append(np.load('{}/{}'.format(trials_dir, trial)))
+
+    results = np.mean(results, axis=0)
+    plt.plot(results)
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_dir('gridworld_cross_entropy_trials')
