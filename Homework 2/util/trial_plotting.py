ace799f1a35f048fa224eba5c8a211da85167aac
Daksh Jotwani <dakshjay@gmail.com>
2018-10-13 23:34:38 -0400

    Add textual info about shape and max value to plots

--- Homework 2/util/trial_plotting.py
+++ Homework 2/util/trial_plotting.py
@@ -17,18 +17,20 @@ def plot_dir(trials_dir):
         [np.load('{}/{}'.format(trials_dir, trial)) for trial in os.listdir(trials_dir)]
     )
 
-    print('shape = {}'.format(results.shape))
-    print('max = {}'.format(np.max(np.mean(results, axis=0))))
-
     plt.errorbar(range(1, results.shape[1] + 1),
         np.mean(results, axis=0), np.std(results, axis=0), ecolor='yellow')
+    text = '{}\n{}'.format(
+        'Shape: {}'.format(results.shape),
+        'Max: {}'.format(np.max(np.mean(results, axis=0)))
+    )
+    plt.gcf().text(0, 0, text)
 
     plt.show()
 
 
 if __name__ == '__main__':
-    plot_dir('../method/cross_entropy/gridworld_cross_entropy_trials')
-    plot_dir('../method/cross_entropy/cartpole_cross_entropy_trials')
+    # plot_dir('../method/cross_entropy/gridworld_cross_entropy_trials')
+    # plot_dir('../method/cross_entropy/cartpole_cross_entropy_trials')
 
     plot_dir('../method/hill_climbing/gridworld_hill_climbing_trials')
-    plot_dir('../method/hill_climbing/cartpole_hill_climbing_trials')
+    # plot_dir('../method/hill_climbing/cartpole_hill_climbing_trials')
