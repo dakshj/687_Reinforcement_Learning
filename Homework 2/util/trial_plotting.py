66bee7665193f6ac3a64ed6ced9694db1a2e97f0
Daksh Jotwani <dakshjay@gmail.com>
2018-10-13 23:28:57 -0400

    Add standard deviation to plot

--- Homework 2/util/trial_plotting.py
+++ Homework 2/util/trial_plotting.py
@@ -5,22 +5,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_dir(trials_dir):
-    results = []
-
-    for trial in os.listdir(trials_dir):
-        results.append(np.load('{}/{}'.format(trials_dir, trial)))
-
-    results = np.mean(results, axis=0)
-    plt.plot(results)
-    plt.show()
-
-
 def save_trial(np_arr, trial_dir: str):
     if not os.path.exists(trial_dir):
         os.mkdir(trial_dir)
 
     np.save('{}/trial_{}'.format(trial_dir, time.time()), np_arr)
+
+
+def plot_dir(trials_dir):
+    results = np.array(
+        [np.load('{}/{}'.format(trials_dir, trial)) for trial in os.listdir(trials_dir)]
+    )
+
+    print('shape = {}'.format(results.shape))
+    print('max = {}'.format(np.max(np.mean(results, axis=0))))
+
+    plt.errorbar(range(1, results.shape[1] + 1),
+        np.mean(results, axis=0), np.std(results, axis=0), ecolor='yellow')
+
+    plt.show()
 
 
 if __name__ == '__main__':
