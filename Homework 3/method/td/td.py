e635cacd02e94285e98f146c46c585ab9afa69e3
Daksh Jotwani <dakshjay@gmail.com>
2018-11-06 14:01:15 -0500

    Implement td.py

--- Homework 3/method/td/td.py
+++ Homework 3/method/td/td.py
@@ -1,5 +1,6 @@
 import itertools
 import math
+from operator import itemgetter
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,37 +17,59 @@ FOURIER_BASIS_VALUES = [3, 5]
 ENV_VALUES = [cartpole.ENV, gridworld.ENV]
 
 
-def execute(env: str, alpha: float, fourier_basis_n: int = None,
-            num_episodes_for_weight_update: int = WEIGHT_UPDATE_NUM_EPISODES,
-            num_episodes_for_mse_calc: int = MSE_CALC_NUM_EPISODES) -> float:
-    policy = get_policy(env)
+def execute(alpha: float, agent_execute_func, fourier_basis_n: int = None,
+            weight_update_episodes: int = WEIGHT_UPDATE_NUM_EPISODES,
+            mse_calc_episodes: int = MSE_CALC_NUM_EPISODES) -> float:
+    """
 
-    fourier_arr = get_nth_order_fourier_basis(policy, fourier_basis_n)
-    phi = [math.cos(math.pi * np.dot(c_i, policy)) for c_i in fourier_arr]
-    weights = get_random_weights(np.shape(phi)[0])
+    :return: Mean-squared TD error over all episodes and all time steps per episode
+    """
+    fourier_arr = None
+    weights = None
+    state_prev = None
+    v_state_prev = None
 
-    for _ in range(num_episodes_for_weight_update):
-        # TODO
-        td_err = None
-        weights += alpha * td_err * None
+    # Save TD Errors for each episode X each time step
+    td_errs = []
 
-    mse = 0.
+    for episode, state, reward, gamma in agent_execute_func(
+            weight_update_episodes + mse_calc_episodes):
 
-    for _ in range(num_episodes_for_mse_calc):
-        # TODO Calculate td_err
-        td_err = 0.
+        if fourier_arr is None:
+            fourier_arr = get_nth_order_fourier_basis(state, fourier_basis_n)
 
-        mse += td_err
+        phi = np.cos(math.pi * np.dot(fourier_arr, state))
 
-    return mse / num_episodes_for_mse_calc
+        if weights is None:
+            weights = get_random_weights(np.shape(phi)[0])
 
+        v_state = np.dot(weights, phi)
 
-def get_policy(env: str):
-    if env == gridworld.ENV:
-        return gridworld.generate_random_gridworld_tabular_softmax_policy()
+        # This is the first iteration, so can't calculate TD error yet
+        # Simply store previous values and continue to next iteration
+        if state_prev is None:
+            state_prev = state
+            v_state_prev = v_state
+            continue
 
-    if env == cartpole.ENV:
-        return cartpole.generate_random_cartpole_policy()
+        td_err = reward + gamma * v_state - v_state_prev
+
+        if 0 <= episode < weight_update_episodes:
+            weights += alpha * td_err * phi
+
+        elif weight_update_episodes <= episode < mse_calc_episodes:
+            td_errs.append((episode, td_err))
+
+    # Convert list of tuples to 2D array of episodes X time stamps
+    td_errs = np.array(
+        [
+            [x for _, x in group]
+            for _, group in itertools.groupby(td_errs, itemgetter(0))
+        ]
+    )
+
+    # Calculate and return mean-squared TD error
+    return np.average(np.sum(td_errs ** 2, axis=1))
 
 
 def get_nth_order_fourier_basis(policy: np.ndarray, fourier_basis_n: int) -> list:
@@ -74,6 +97,13 @@ def plot(data):
     plt.show()
 
 
+def get_agent_execute_func(env: str):
+    if env == cartpole.ENV:
+        return cartpole.execute
+    elif env == gridworld.ENV:
+        return gridworld.execute
+
+
 def execute_all():
     results = []
 
@@ -82,7 +112,8 @@ def execute_all():
 
             curr_results = []
             for alpha in ALPHA_VALUES:
-                mse = execute(env, alpha, fourier_basis_n=fourier)
+                mse = execute(alpha, fourier_basis_n=fourier,
+                    agent_execute_func=get_agent_execute_func(env))
                 curr_results.append((math.log(alpha, 10), mse))
 
             results.append(curr_results)
