Make td_errs a pre-defined size ndarray, Fix float overflow issues

Homework 3/method/td/td.py
@@ -1,6 +1,5 @@
 import itertools
 import math
-from operator import itemgetter
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,6 +14,7 @@ WEIGHT_UPDATE_NUM_EPISODES = 100
 MSE_CALC_NUM_EPISODES = 100
 FOURIER_BASIS_VALUES = [3, 5]
 ENV_VALUES = [cartpole.ENV, gridworld.ENV]
+MAX_TIME_STEPS = 1010
 
 
 def execute(alpha: float, agent_execute_func, fourier_basis_n: int = None,
@@ -29,7 +29,7 @@ def execute(alpha: float, agent_execute_func, fourier_basis_n: int = None,
     v_prev = None
 
     # Save TD Errors for each episode X each time step
-    td_errs = []
+    td_errs = np.zeros((mse_calc_episodes, MAX_TIME_STEPS))
 
     for time_step, episode, state, reward, gamma in agent_execute_func(
             weight_update_episodes + mse_calc_episodes):
@@ -44,26 +44,19 @@ def execute(alpha: float, agent_execute_func, fourier_basis_n: int = None,
         if weights is None:
             weights = get_random_weights(np.shape(phi)[0])
 
-        v = np.dot(weights, phi)
+        v = round(float(np.dot(weights, phi)), ndigits=2)
 
         if time_step != 0:
-            td_err = reward + gamma * v - v_prev
+            td_err = round(reward + gamma * v - v_prev, ndigits=2)
 
             if 0 <= episode < weight_update_episodes:
                 weights += alpha * td_err * phi
 
-            elif weight_update_episodes <= episode < mse_calc_episodes:
-                td_errs.append((episode, td_err))
+            elif weight_update_episodes <= episode < \
+                    (weight_update_episodes + mse_calc_episodes):
+                td_errs[episode - weight_update_episodes, time_step] = td_err
 
         v_prev = v
-
-    # Convert list of tuples to 2D array of episodes X time stamps
-    td_errs = np.array(
-        [
-            [x for _, x in group]
-            for _, group in itertools.groupby(td_errs, itemgetter(0))
-        ]
-    )
 
     # Calculate and return mean-squared TD error
     return np.average(np.sum(td_errs ** 2, axis=1))
@@ -81,9 +74,7 @@ def plot(data):
     labels = ['CartPole Fourier Basis 3', 'CartPole Fourier Basis 5', 'GridWorld']
     for row, label in zip(data, labels):
         x, y = map(list, zip(*row))
-        print(type(x))
-        print(x)
-        plt.plot(x, y, label=label)
+        plt.plot([round(x_ith) for x_ith in x], y, label=label)
         plt.xticks(np.arange(min(x), max(x) + 1))
 
     plt.legend(loc='upper left')