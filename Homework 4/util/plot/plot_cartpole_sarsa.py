88dce0fd259219dff315ad5bd6a2f97025918485
Daksh Jotwani <dakshjay@gmail.com>
2018-11-20 03:51:45 -0500

    Add new line for easier folder name pasting

--- Homework 4/util/plot/plot_cartpole_sarsa.py
+++ Homework 4/util/plot/plot_cartpole_sarsa.py
@@ -1,4 +1,5 @@
 from util.plot.plot_trials import plot_dir
 
 if __name__ == '__main__':
-    plot_dir('../../method/sarsa/cartpole__sarsa__e=0.3__a=0.1__f=3')
+    plot_dir('../../method/sarsa/'
+             'cartpole__sarsa__e=0.6__a=0.1__f=3')
--- Homework 4/util/plot/plot_gridworld_sarsa.py
+++ Homework 4/util/plot/plot_gridworld_sarsa.py
@@ -1,4 +1,5 @@
 from util.plot.plot_trials import plot_dir
 
 if __name__ == '__main__':
-    plot_dir('../../method/sarsa/gridworld__sarsa__e=0.3__a=0.1')
+    plot_dir('../../method/sarsa/'
+             'gridworld__sarsa__e=0.3__a=0.1')
