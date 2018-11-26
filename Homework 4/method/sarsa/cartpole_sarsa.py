34335e6f29ba7142f1d54a0271e1bd3e7cb32210
Daksh Jotwani <dakshjay@gmail.com>
2018-11-25 22:11:53 -0500

    Fix hyperparams for cartpole_sarsa.py

--- Homework 4/method/sarsa/cartpole_sarsa.py
+++ Homework 4/method/sarsa/cartpole_sarsa.py
@@ -8,24 +8,23 @@ from util.random_hyperparameter_search import random_hyperparameter_search
 
 TRIALS = 100
 
-# ALL   = [0.1, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6]
-EPSILON = [0.35]
+# ALL   = [0.1, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8]
+EPSILON = [0.95]
 
 # `1` means no decay
-# ALL         = [1, 0.98]
+# ALL         = [1, 0.99, 0.98, 0.95, 0.9]
 EPSILON_DECAY = [0.98]
 
-# BAD = [0.1]
-# ALL = [0.000001, 0.00001, 0.0001, 0.00005, 0.001, 0.0005, 0.1]
-ALPHA = [0.001]
+# ALL = [0.000001, 0.00001, 0.0001, 0.00005, 0.001, 0.0005, 0.1, 0.01, 0.05, 0.3]
+ALPHA = [0.01]
 
-# ALL               = [3, 4, 5]
-FOURIER_BASIS_ORDER = [5]
+# ALL               = [3, 4, 5, 8]
+FOURIER_BASIS_ORDER = [3]
 
 # ALL    = [100, 200]
-EPISODES = [400]
+EPISODES = [200]
 
-SKIP_EXISTING_PATH = True
+SKIP_EXISTING_PATH = False
 
 
 def execute():
