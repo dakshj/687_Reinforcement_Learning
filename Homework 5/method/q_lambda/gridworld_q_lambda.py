0b13e26989de18072f1bc6ac03e59ac894b066c8
Daksh Jotwani <dakshjay@gmail.com>
2018-12-04 14:59:03 -0500

    Finalize hyperparams for gridworld_q_lambda.py

--- Homework 5/method/q_lambda/gridworld_q_lambda.py
+++ Homework 5/method/q_lambda/gridworld_q_lambda.py
@@ -6,23 +6,23 @@ from method.q_lambda.q_lambda import q_lambda
 from util.plot.plot_trials import save_trial
 from util.random_hyperparameter_search import random_hyperparameter_search
 
-TRIALS = 15
+TRIALS = 100
 
 # ALL   = [0.98, 0.95, 0.9, 0.8, 0.4, 0.3, 0.1]
-EPSILON = [0.98, 0.95, 0.9, 0.8, 0.4, 0.3, 0.1]
+EPSILON = [0.98]
 
 # `1` means no decay
 # ALL         = [1, 0.99, 0.98, 0.95]
-EPSILON_DECAY = [1, 0.99, 0.98, 0.95]
+EPSILON_DECAY = [0.95]
 
 # ALL = [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.3, 0.75, 0.25, 0.2]
-ALPHA = [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.3, 0.75, 0.25, 0.2]
+ALPHA = [0.3]
 
 # ALL    = [150]
 EPISODES = [150]
 
 # ALL  = [0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.8, 0.9]
-LAMBDA = [0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 0.8, 0.9]
+LAMBDA = [0.1]
 
 SKIP_EXISTING_PATH = True
 
@@ -33,7 +33,7 @@ def execute():
                     ALPHA, EPISODES, LAMBDA):
         episodes = int(episodes)
 
-        trials_dir = '{}__q_learning__e={}__d={}__a={}__ep={}__l={}' \
+        trials_dir = '{}__q_lambda__e={}__d={}__a={}__ep={}__l={}' \
             .format(gridworld.ENV, epsilon, epsilon_decay, alpha, episodes, lambda_)
 
         # Skipping existing dirs helps in parallelization by skipping
